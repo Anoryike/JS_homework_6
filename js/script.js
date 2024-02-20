2d5a19c3110eeacded8288e80646f65eb104bd48
Anoryike <149113497+Anoryike@users.noreply.github.com>
2024-02-21 00:13:02 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+// class Abonent {
+//   constructor(props) {
+//     this.abonent = props.abonent;
+//   }
+
+//   name = "";
+//   number = "";
+
+//   set abonent(newAbonent) {
+//     const abonentRow = newAbonent.split(" ");
+//     this.name = abonentRow[0];
+//     this.number = abonentRow[1];
+//   }
+
+//   get abonent() {
+//     return `Abonent name: ${this.name}, Abonent number: ${this.number}`;
+//   }
+// }
+
+// const abonent1 = new Abonent({
+//   abonent: "Maks +380973424365",
+// });
+
+// const abonent2 = new Abonent({
+//   abonent: "Ann +380966512455",
+// });
+
+// const abonent3 = new Abonent({
+//   abonent: "Ann +380981453112",
+// });
+
+// console.log(abonent1);
+// console.log(abonent2);
+// console.log(abonent3);
